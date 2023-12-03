d70ddddbb7ecb3e178d9bf074a4caf4e0347bf9a
ial00003 <ial00003@students.stir.ac.uk>
2023-12-03 10:22:27 +0000

    boiler code for day 3

---
+++ 3/part_1.py
@@ -0,0 +1,9 @@
+import re
+input_file = open("test.txt", "r")
+lines = input_file.readlines()
+
+for line in eachline(input_file):
+    
+print(lines.type())
+#print(lines.length())
+#arr = [[0 for i in range(cols)] for j in range(rows)]
