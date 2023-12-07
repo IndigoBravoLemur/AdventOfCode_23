cb5db860a1908e964b8864999f197a1cd166bd08
Ian <>
2023-12-07 23:47:09 +0000

    giving me a headache, 7 part 1 nearly there

--- 7/part_1.py
+++ 7/part_1.py
@@ -1,19 +1,4 @@
-# list of hands, 5 cards each - AKQJT 9-2 A high
-#       [['1','2','3','4','5'],['1','2','3','4','5']]
-# Hands have ONE type
-# order based on strength
-# reduce ? - no , applies a function to eachelement in list and reduces to a singe output ? 
-#can get strength of hand by reducing on the list  is that fast
-hands = [line.split() for line in open('test.txt')]
-
-# An array of each type containing tuples of hand and bet size ?
-five_kind = []
-four_kind = []
-full_house = []
-three_kind = []
-two_pair = []
-one_pair = []
-high_card = []
+hands = [line.split() for line in open('test2.txt')]
 
 from collections import Counter
 
@@ -26,12 +11,28 @@ def most_frequent_character(hand):
     ## However, when used as a key function for max, the max function implicitly calls .get on each key without passing any additional arguments.
     most_frequent = max(char_count, key=char_count.get)
     frequency = char_count[most_frequent]
-    if frequency == 3:
-        print("")
+    if frequency == 3 or frequency == 2:
+        char_count.pop(most_frequent)
+        second_most_frequent = max(char_count, key=char_count.get)
+        second_frequency = char_count[second_most_frequent]
+        if second_frequency == 2:
+            frequency = frequency+(second_frequency/10)
         
     return most_frequent, frequency
 
 for hand in hands:
-    result, frequency = most_frequent_character(hand[0])
-    hand.append(frequency)
-    print(f"The most frequent character in '{hand}' is: {result} {frequency}")
+    result, strength = most_frequent_character(hand[0])
+    hand.append(strength)
+    print(f"The most frequent character in '{hand}' is: {result} {strength}")
+
+# https://stackoverflow.com/questions/64576903/how-to-sort-the-list-in-custom-order-in-python
+# Create a dictionary of cards to values to use for sorting
+card_weighting = {card:iterator for iterator,card in enumerate(['1','2','3','4','5','6','7','8','9','T','J','Q','K','A'])}
+
+
+# Sort the list based on the second index in each sublist, ie the strength.
+# In case of a tie, which will occur on all instances of the same card type, it sorts based on the second condition, the custom sorting function
+# Not working. Sorting the strength but not the card weighting
+hands.sort(key=lambda card: (int(card[2]), card_weighting.get(card[0], len(card_weighting))))
+
+print(hands)
