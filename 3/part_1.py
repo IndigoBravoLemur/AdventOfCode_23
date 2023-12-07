92e7078ead97da130cd6197ad9caa254b7049634
Ian <>
2023-12-07 09:27:44 +0000

    Updating day 6, day 3

--- 3/part_1.py
+++ 3/part_1.py
@@ -1,72 +1,23 @@
 def main():
-    # List comprehension for reading lines and stripping /n at the end
-    # list = return list of the output of the expression line.rstrip() for each line (item) in the txt file (iterable).
     lines = [line.rstrip() for line in open('test2.txt')]
-
-    # List Comprehension :: newlist = [%expression% for %item% in %iterable% if %condition% == True]
-    # Create a list of the output of the expression 'character for....' for each line in the lines list
-    #       The expression returns a list of characters for each character in the provided line 
     line_list = [[character for character in line] for line in lines]
-    parts = []
-    # For each line 
-    for line_number in range(len(line_list)):
-        print("Checking line number {}".format(line_number))
-        # For each character in line
-        for index in range(len(line_list[line_number])):
-            print("Checking character {}".format(line_list[line_number][index]))
-            is_part = False
-            special_characters = "!@#$%^&*()-+?_=,<>/"
 
-            if line_list[line_number][index].isdigit():
-                char_is_digit = True
+    for line_number in range(len(line_list)):    
+        result = [check_is_part(line_list, line_number, character) for character in range(len(line_list[line_number]))]
 
-            # For each row above, current and below the current line number
-            for i in range(-1,2):
-                print("checkng line {}".format(i))
-                current_number = ""
-                # Skip if at the beginning or end of line
-                if i < len(line_list[line_number+i]) or i > len(line_list)-1:
-                    continue
+    print(result)
 
-                # Check characters of all adjacent to the given char
-                # Starting at index-1, ending when it reaches index+2 (not including), add 1 each iteration
-                for i in range(index-1,index+2,1):
-                    if i < 0 or i > len(line_list[line_number])-1:
-                        continue                  
-                    if line_list[line_number][i] in special_characters:
-                        print("{} adjacent to special char".format(line_list[line_number][i]))
-                        is_part=True
-                    #else:
-                        #print("{} not adjacent to special char".format(line_list[line_number][i]))
-            
-            if is_part:
-                print("{} adjacent to special char".format(line_list[line_number][index]))
-            # If character is digit add to current digit list
-            if char_is_digit:
-                current_number += str(line_list[line_number][index])
-            # Else if character is not a digit
+def check_is_part(line_list, line_number, index):
+        special_characters = "!@#$%^&*()-+?_=,<>/"
+        for i in range(index-1,index+2,1):
+            if i < 0 or i > len(line_list[line_number])-1:
+                continue               
+            if line_list[line_number][i] in special_characters:
+                print("adjacent to special char")
             else:
-                if current_number and is_part:
-                    parts.append(current_number)
-                    current_number = ""
+                print("not adjacent to special char")
+            print(line_list[line_number][i])
+            is_part=True
 
-                        
-
-        # Current row
-        # Below row
-
-##  1 . . . .
-##  . 4 . 4 5
-##  3 . . . *
-# 
-# if number AND adjacent is part is true 
-#    add to curr_num 
-# else
-#       if curr_num exists AND part is true
-#           add curr_num to list of known good
-#           set curr_num=""
-#           set adjacent to false
-#
-#           
 if __name__ == "__main__":
     main()
--- 6/part_2.py
+++ 6/part_2.py
@@ -5,14 +5,18 @@ data = [line.split() for line in open('input.txt')]
 #data = [del a[0] for a in data]
 # This is effectively the same thing, returning a list with elements from position 1 onwards, ie, without [0]
 data = [a[1:] for a in data]
+data[0] = "".join(data[0])
+data[1] = "".join(data[1])
 print(data)
 answer = 1
-for race_number in range(len(data[0])):
+
+for race_number in range(len(data)):
     print(f"Race #{race_number}")
-    race_length = int(data[0][race_number])
-    record_distance = int(data[1][race_number])
+    race_length = int(data[0])
+    record_distance = int(data[1])
+    print(f'length - {race_length} distance - {record_distance}')
     ways_of_winning = 0
-    for i in range(race_length): # each i is time held down
+    for i in range(race_length): # Each i is time held down
         # Length travelled = Speed * (Race Length - time held button down)
         length_travelled = i * (race_length-i) 
         if length_travelled > record_distance:
