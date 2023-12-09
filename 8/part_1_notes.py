a805a7bccbba50d01c9cc63f7d356611ae88aec9
Ian <>
2023-12-09 00:43:30 +0000

    Completed d8, kind of

--- 8/part_1_notes.py
+++ 8/part_1_notes.py
@@ -1,13 +1,18 @@
 with open('input.txt') as f:
     instructions = f.readline()
     f.readline()
+    print(instructions)
     instructions_bin = [0 if c == 'L' else 1 for c in instructions.strip()]
+    print(instructions_bin)
     node_list = f.readlines()
 
+# Translation table is better than .replace as it looks for chars, not substrings, and can do multiple at once
 translation_table = str.maketrans({'=': None, '(': None, ',': None, ')': None})
+# Can this be made into a dict instead with tuple ? 
 desert_map = [line.translate(translation_table).split() for line in node_list]
 desert_dict = {items[0]: tuple(items[1:]) for items in desert_map}
-#desert_dict = {line.translate(translation_table).split()[0]: tuple(line.translate(translation_table).split()[1:]) for line in node_list}
+
+# desert_dict = {line.translate(translation_table).split()[0]: tuple(line.translate(translation_table).split()[1:]) for line in node_list}
 ### line.translate(translation_table).split()[0] extracts the first item for the key.
 ### tuple(line.translate(translation_table).split()[1:]) extracts the rest of the items as a tuple for the value.
 
@@ -19,12 +24,16 @@ def move(node, ii):
     try:
         node = desert_dict[node][instructions_bin[ii]]
         ii+=1
-        steps += 1 
+        steps += 1
+        
     except IndexError:
         ii = 0
-        return node, ii 
+        return node, ii
+    
+    print(f"node is {node} iterator is {ii}")
     return node, ii
 
 while node != 'ZZZ':   
     node, ii = move(node,ii)
+
 print(steps)
--- 8/part_2.py
+++ 8/part_2.py
@@ -1,3 +1,4 @@
+# This works to brute force but is unreasonable to complete the task with. needs the LCM solution
 with open('input.txt') as f:
     instructions = f.readline()
     f.readline()
@@ -10,21 +11,26 @@ desert_dict = {items[0]: tuple(items[1:]) for items in desert_map}
 #desert_dict = {line.translate(translation_table).split()[0]: tuple(line.translate(translation_table).split()[1:]) for line in node_list}
 ### line.translate(translation_table).split()[0] extracts the first item for the key.
 ### tuple(line.translate(translation_table).split()[1:]) extracts the rest of the items as a tuple for the value.
-
-node = "AAA"
+nodes = [node for node in desert_dict if (node[2]=='A')]
 ii=0
 steps=0
-def move(node, ii):
+
+def move(nodes):
     global steps
+    global ii
     try:
-        node = desert_dict[node][instructions_bin[ii]]
+        # For each node, new node = desert_dict
+        for i,node in enumerate(nodes):
+            nodes[i] = desert_dict[node][instructions_bin[ii]]
         ii+=1
         steps += 1 
     except IndexError:
         ii = 0
-        return node, ii 
-    return node, ii
+        return nodes
+    return nodes
+print(nodes)
+while [node for node in nodes if (node[2]!='Z')]:
+    nodes = move(nodes)
+    print(nodes)
 
-while node != 'ZZZ':   
-    node, ii = move(node,ii)
 print(steps)
